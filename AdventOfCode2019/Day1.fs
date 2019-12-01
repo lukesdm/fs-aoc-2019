913e821a3b06053b14c2aec6e3dbbc007ffa4cbd
Luke McQuade <git@lukemcquade.co.uk>
2019-12-01 21:51:17 +0000

    tests pass

--- AdventOfCode2019/Day1.fs
+++ AdventOfCode2019/Day1.fs
@@ -27,6 +27,12 @@ let total1 = Array.sumBy calcFuel1 input
 // The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
 // What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
 
-let calcFuel2 moduleMass = 0 // TODO
+let calcFuel2 moduleMass =
+    let mutable fuelInc = (moduleMass / 3) - 2
+    let mutable fuelTotal = 0
+    while fuelInc > 0 do
+        fuelTotal <- fuelTotal + fuelInc
+        fuelInc <- (fuelInc / 3) - 2
+    fuelTotal
 
 let total2 = Array.sumBy calcFuel2 input
