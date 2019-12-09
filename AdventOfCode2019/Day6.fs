3ac23c345f49f00e37220ee7a1f0e487eb060530
Luke McQuade <git@lukemcquade.co.uk>
2019-12-09 14:34:13 +0000

    Day 6 part 1 - reworking attempt - revisit later

--- AdventOfCode2019/Day6.fs
+++ AdventOfCode2019/Day6.fs
@@ -14,7 +14,6 @@ let tokenize (orbitDesc: string) =
     (ts.[0], ts.[1])
     
 type ID = string
-//type Orbit = { id: ID; parentId: ID option; orbitCount: int }
 type Orbit = { id: ID; parentId: ID option; }
 type Orbits = Dictionary<ID, Orbit>
 
@@ -30,21 +29,21 @@ let parse (orbitDescs: string[]) =
         )
     orbits
     
-// recursive attempt - won't work like this because of assumed list order. 
+// recursive attempt - terrible performance (suspect Map.Add)
 //type Orbits = Map<ID, Orbit>
+//let parse (orbitDescs: seq<string>) =
 //let parse (orbitDescs: string[]) =
-//    let root = { id = "COM"; parentId = None; orbitCount = 0 } 
-//    let rec parse orbitDescs (orbits: Orbits) =
+//    let root = { id = "COM"; parentId = None; } 
+//    let rec parseI orbitDescs (orbits: Orbits) =
 //        if Seq.isEmpty orbitDescs then
 //            orbits
 //        else
-//            let id, parentId = Seq.head orbitDescs |> tokenize
-//            let parent = orbits.[parentId]
-//            let orbit = { id = id; parentId = Some parentId; orbitCount = 1 + parent.orbitCount } 
-//            parse (Seq.tail orbitDescs) (orbits.Add(orbit.id, orbit))
-//    parse orbitDescs (Map.ofList [root.id, root])
+//            let parentId, id = Seq.head orbitDescs |> tokenize
+//            let orbit = { id = id; parentId = Some parentId; } 
+//            printfn "T = %A it = %d" System.DateTime.Now it
+//            parseI (Seq.tail orbitDescs) (orbits.Add(orbit.id, orbit))
+//    parseI orbitDescs (Map.ofList [root.id, root])
 
-// TODO: memoized version - orbitCounts map
 let calcOrbitCount (orbits: Orbits) (orbit: Orbit) =
     let rec countOrbits (orbit: Orbit) count =
         if orbit.parentId.IsNone then count // COM root
@@ -52,9 +51,27 @@ let calcOrbitCount (orbits: Orbits) (orbit: Orbit) =
             countOrbits orbits.[orbit.parentId.Value] (count + 1)
     countOrbits orbit 0 
 
+// memoization attempt - returns wrong results TODO: Fix
+//type OrbitCounts = Map<ID, int>
+//let calcOrbitCounts (orbits: Orbits) : OrbitCounts =
+//    let rec countOrbits (orbit: Orbit) (orbitCounts: OrbitCounts) =
+//        if orbit.parentId.IsNone then orbitCounts // root/COM
+//        else
+//            let parent = orbits.[orbit.parentId.Value]
+//            let parentCount = Map.tryFind parent.id orbitCounts
+//            if parentCount.IsSome then orbitCounts.Add (orbit.id, 1 + parentCount.Value)
+//            else countOrbits parent orbitCounts
+//    
+//    let orbitCounts = orbits.Values |> Seq.fold ( fun (orbitCountsAcc: OrbitCounts) orbit ->
+//            countOrbits orbit orbitCountsAcc ) (Map.ofList [ ("COM", 0) ])
+//    orbitCounts
+//    
+//let calcChecksum (orbits: Orbits) = 
+//    orbits |> calcOrbitCounts
+//    |> Map.fold ( fun total _ count -> total + count ) 0
+
 let calcChecksum (orbits: Orbits) = 
     orbits
-    //|> Seq.fold (fun total kvp -> total + kvp.Value.orbitCount) 0
     |> Seq.fold (fun total kvp -> total + (calcOrbitCount orbits kvp.Value)) 0
 
 // TESTS
@@ -97,5 +114,5 @@ let execute () =
         |> tokenizeFile
         |> parse
         |> calcChecksum
-    
+        
     printfn "Day 6 part 1 result: %d" result // 301100 confirmed correct.
