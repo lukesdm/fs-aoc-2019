3a9cff7c24400617635b3949e34d5946855014ce
Luke McQuade <git@lukemcquade.co.uk>
2019-12-10 20:55:01 +0000

    Day 9 - 64 bit machine

--- AdventOfCode2019/Day9.fs
+++ AdventOfCode2019/Day9.fs
@@ -12,15 +12,20 @@ open System.IO
 // *****
 
 // Program/memory
-type Program = int[]
+//type Program = int64[]
 
-type Input = Queue<int>
-type Output = Queue<int>
+// sparse vector
+type Program(initialMemory: int64[]) =  
+    member this.Item
+        with get (index: int64) = initialMemory.GetValue(index) :?> int64
+        and set (index: int64) (value: int64) = initialMemory.SetValue(value, index)
+type Input = Queue<int64>
+type Output = Queue<int64>
     
 type ParamMode =
     | Position = 0
     | Immediate = 1
-type Value = int
+type Value = int64
 type Param = ParamMode * Value
 type Params =
     | Zilch // Zero (avoid naming conflict)
@@ -56,7 +61,7 @@ type Status =
     | Halted
     
 type NewState =
-    | PCOverride of int
+    | PCOverride of int64
     | Status of Status
 
 //ABCDE
@@ -68,25 +73,26 @@ type NewState =
 // A - mode of 3rd parameter,  0 == position mode,
 //                                  omitted due to being a leading zero
 let pad0 n (digits: int[]) =
-    Array.append [|for _ in 1..(n-digits.Length) -> 0 |] digits
+    Array.append [|for _ in 1..(n - digits.Length) -> 0 |] digits
 
 let parseInst (inst: int) : OpCode * ParamMode[] =
-    let digits = intToDigits inst |> pad0 5 // for now, always pad to 5 and ignore surplus modes 
-    let opCode = enum<OpCode> (10*digits.[3] + digits.[4])
+    // For now, expect Int32-sized instruction
+    let digits = intToDigits inst |> pad0 5 // for now, just pad to 5 and ignore surplus modes 
+    let opCode = enum<OpCode> (digits.[4] + 10 * digits.[3])
     let paramModes: ParamMode[] = digits.[..2] |> Array.rev |> Array.map enum<ParamMode>
     (opCode, paramModes)
 
-let parse (program: Program) (pc: int) : Instruction =
-    let (opCode, modes) = parseInst program.[pc]
+let parse (program: Program) (pc: int64) : Instruction =
+    let (opCode, modes) = parseInst (int program.[pc])
     let def = { opCode = opCode; opParams = Zilch }
     match opCode with
     | OpCode.Halt -> def
     | OpCode.Input | OpCode.Output ->
-        { def with opParams = One (modes.[0], program.[pc+1])  }
+        { def with opParams = One (modes.[0], program.[pc + 1L])  }
     | OpCode.JumpIfFalse | OpCode.JumpIfTrue ->
-        { def with opParams = Two ((modes.[0], program.[pc+1]), (modes.[1], program.[pc+2])) }
+        { def with opParams = Two ((modes.[0], program.[pc + 1L]), (modes.[1], program.[pc + 2L])) }
     | OpCode.Add | OpCode.Multiply | OpCode.LessThan | OpCode.Equals ->
-        { def with opParams = Three ((modes.[0], program.[pc+1]), (modes.[1], program.[pc+2]), (modes.[2], program.[pc+3])) }
+        { def with opParams = Three ((modes.[0], program.[pc + 1L]), (modes.[1], program.[pc + 2L]), (modes.[2], program.[pc + 3L])) }
     | _ -> failwith "Bad input format"
 
 let getArg (mem: Program) (p:Param) =
@@ -107,17 +113,17 @@ let eval (program: Program) (instruction: Instruction) (input: Input) (output: O
         match opCode with
         | OpCode.Add -> program.[dest] <- in1Val + in2Val
         | OpCode.Multiply -> program.[dest] <- in1Val * in2Val
-        | OpCode.LessThan -> program.[dest] <- if in1Val < in2Val then 1 else 0
-        | OpCode.Equals -> program.[dest] <- if in1Val = in2Val then 1 else 0 
+        | OpCode.LessThan -> program.[dest] <- if in1Val < in2Val then 1L else 0L
+        | OpCode.Equals -> program.[dest] <- if in1Val = in2Val then 1L else 0L
         | _ -> failwith "Unsupported opCode"
     | opCode, Two (param1, param2) ->
         let test = getArg param1
         let newPc = getArg param2
         match opCode with
         | OpCode.JumpIfTrue ->
-            if test <> 0 then newState <- Some (PCOverride newPc)
+            if test <> 0L then newState <- Some (PCOverride newPc)
         | OpCode.JumpIfFalse ->
-            if test = 0 then newState <- Some (PCOverride newPc)
+            if test = 0L then newState <- Some (PCOverride newPc)
         | _ -> failwith "Unsupported opCode"
     | opCode, One (param) ->
         match opCode with
@@ -150,13 +156,13 @@ let run (program : Program) (input: Input) (output: Output) initPc =
         match newState with
         | Some (PCOverride pcO) -> pc <- pcO 
         | Some (Status s) -> status <- s
-        | None -> pc <- (pc + instruction.Length)
+        | None -> pc <- (pc + int64 instruction.Length)
         
     (status, pc)
 
 let parseProgDesc (desc: string) : Program =
-    desc.Split(",")
-    |> Array.map System.Int32.Parse
+    let mem = desc.Split(",") |> Array.map Int64.Parse
+    new Program(mem)
     
 // *****
 
@@ -166,13 +172,38 @@ let runTests() =
         let prog =
             "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
             |> parseProgDesc
-        let expected = [| 109; 1; 204; -1; 1001; 100; 1; 100; 1008; 100; 16; 101; 1006; 101; 0; 99 |]
+        let expected = [| 109L; 1L; 204L; -1L; 1001L; 100L; 1L; 100L; 1008L; 100L; 16L; 101L; 1006L; 101L; 0L; 99L |]
         let output = new Output()
-        let _ = run prog (new Input()) output 0
+        let _ = run prog (new Input()) output 0L
         let actual = output.ToArray()
+        assert (expected = actual)
+        
+    let ``example 2 - large multiply`` () =
+        // "should output a 16-digit number"
+        let prog =
+            "1102,34915192,34915192,7,4,7,99,0"
+            |> parseProgDesc
+        let expected = 16
+        let output = new Output()
+        let _ = run prog (new Input()) output 0L
+        let result = output.Dequeue()
+        let actual = result.ToString().Length
+        assert (expected = actual)
+        
+    let ``example 3 - large output`` () =
+        // "should output the large number in the middle"
+        let prog =
+            "104,1125899906842624,99"
+            |> parseProgDesc
+        let expected = 1125899906842624L
+        let output = new Output()
+        let _ = run prog (new Input()) output 0L
+        let actual = output.Dequeue()
         assert (expected = actual)
     
     ``example 1 - quine``()
+    ``example 2 - large multiply`` ()
+    ``example 3 - large output`` ()
     
 let execute() =
     let prog = File.ReadAllText "Auxi\day9-input.txt" |> parseProgDesc
--- AdventOfCode2019/Shared.fs
+++ AdventOfCode2019/Shared.fs
@@ -6,6 +6,11 @@ let intToDigits (n: int) =
     string n |> Seq.map (fun c -> Int32.Parse (c.ToString ()))
     |> Seq.toArray
     
+let int64ToDigits (n: int64) =
+    // TODO: make this less ugly?
+    string n |> Seq.map (fun c -> Int64.Parse (c.ToString ()))
+    |> Seq.toArray
+    
 let stringToDigits (s: string) =
     s |> Seq.map (fun c -> Int32.Parse (c.ToString ()))
     |> Seq.toArray
